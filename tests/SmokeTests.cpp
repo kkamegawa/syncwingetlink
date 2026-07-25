098f0266f2137196294ec01eabe808d5f1b2fdd9
KAMEGAWA Kazushi <1450745+kkamegawa@users.noreply.github.com>
2026-07-25 18:20:59 +0900

    [add]: define core application model
    Closes #23.

--- tests/SmokeTests.cpp
+++ tests/SmokeTests.cpp
@@ -2,6 +2,8 @@
 
 #include <CppUnitTest.h>
 
+#include <core/Model.h>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace syncwingetlink::tests
@@ -12,6 +14,18 @@ public:
     TEST_METHOD(frameworkLoads)
     {
         Assert::IsTrue(true);
+    }
+
+    TEST_METHOD(modelDefaultsAreSafe)
+    {
+        const AppOptions options;
+        const RepairItem repairItem;
+
+        Assert::IsTrue(options.command == AppCommand::Scan);
+        Assert::IsTrue(options.source == PackageSource::Auto);
+        Assert::IsTrue(options.logLevel == LogLevel::Normal);
+        Assert::IsFalse(options.dryRun);
+        Assert::IsTrue(repairItem.status == LinkStatus::Missing);
     }
 };
 } // namespace syncwingetlink::tests
